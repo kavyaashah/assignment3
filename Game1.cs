15bdcf934c62a2b405cf5f5ae3400285173e01b3
Caitlyn Pratt <caitlynpratt@utexas.edu>
2026-02-08 14:29:16 -0600

    putting pieces together

--- Game1.cs
+++ Game1.cs
@@ -23,6 +23,21 @@ public class Game1 : Game
 
     private int _canvasWidth;
     private int _canvasHeight;
+    
+    //bools for display
+    private bool display_unique;
+    private bool _held;
+    private KeyboardState _keyboardState;
+    //variables for storing words counts
+    private string wordfreq;
+    private string[] lines;
+    private string[] words;
+    private Dictionary<string, int> wordCounts;
+    private List<KeyValuePair<string, int>> sortedDict;
+    //create our visualization
+    private Texture2D chart;
+    
+    
 
     private MouseState _previousMouse;
 
@@ -36,6 +51,14 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        
+        //create base for variables
+        display_unique = true;
+        wordCounts = new Dictionary<string, int>();
+        chart = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+        chart.SetData<Color>(new Color[] { Color.White });//initialize texture color
+        Window.TextInput += TextInputCallback;//take in text input
+        
         _palette = new Color[]
         {
             Color.MediumPurple,
@@ -52,6 +75,53 @@ public class Game1 : Game
 
         base.Initialize();
     }
+    private void TextInputCallback(object sender, TextInputEventArgs args)
+    {
+        //if enter key is pressed, switch displaying bools
+        switch (args.Key)
+        {
+            case Keys.Enter:
+                if (display_unique && !_held)
+                {
+                    display_unique = false;
+                    _held = true;
+                }
+                else if ( !_held)
+                {
+                    display_unique = true;
+                    _held = true;
+                }
+
+                break;
+            
+        }
+
+    }
+    
+    private void BuildWordFrequency()
+    {
+        //to create a list of lines, break text block by newlines
+        lines = wordfreq.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(':');// split lines on semicolon
+
+            if (parts.Length < 2)
+                continue;
+            
+            //first number in list is frequency, second number is count of frequency
+            //.Trim() gets rid of whitespace
+            string freq = parts[0].Trim();
+            int num = int.Parse(parts[1].Trim());
+            
+            //create dictionary
+            wordCounts[freq] = num;
+        }
+        //sort dictionary
+        sortedDict = wordCounts.OrderByDescending(x => x.Value).ToList();
+        
+    }
 
     protected override void LoadContent()
     {
@@ -64,8 +134,17 @@ public class Game1 : Game
         _uniqueWords = File.ReadAllLines(path).ToList();
 
         DisplayUniqueWords();
+        
+        using (var stream = TitleContainer.OpenStream("Content/files/wordTextFrequncy.txt"))
+        using (var reader = new StreamReader(stream))
+        {
+            wordfreq = reader.ReadToEnd();
+        }
+        // build dictionary
+        BuildWordFrequency();
 
     }
+    
 
     private void DisplayUniqueWords()
     {
@@ -112,32 +191,9 @@ public class Game1 : Game
         }
         
     }
-    
-    protected override void Update(GameTime gameTime)
+
+    private void window_DisplayUniqueWords(SpriteBatch _spriteBatch)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
-        // TODO: Add your update logic here
-        MouseState mouse = Mouse.GetState();
-
-        if (mouse.LeftButton == ButtonState.Pressed && _previousMouse.LeftButton == ButtonState.Released)
-        {
-            DisplayUniqueWords();
-        }
-
-        _previousMouse = mouse;
-        
-
-        base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.White);
-        
-        // TODO: Add your drawing code here
-        _spriteBatch.Begin();
         for (int i = 0; i < _words.Count; i++)
         {
             _spriteBatch.DrawString(
@@ -147,9 +203,103 @@ public class Game1 : Game
                 _cloudColors[i]
             );
         }
+    }
+
+    private void DisplayWordFrequency(SpriteBatch _spriteBatch, Texture2D texture)
+    {
+        //if dict not created, exit
+        if (sortedDict == null || sortedDict.Count == 0)
+            return;
+        //variables for display
+        int totalBars = sortedDict.Count;
+        //int s_width = Window.ClientBounds.Width;
+        int s_height = Window.ClientBounds.Height;
+        int spacing = 5;
+        int barWidth = 10;
+        //(s_width - 100) / totalBars
+        int startx = 50;
+        int basey = s_height - 50;
+        int maxBarHeight = s_height - 150;
+        int max = sortedDict[0].Value;
+
+        //create rectangle for each key value pair in sortedDict
+        for (int i = 0; i < totalBars; i++)
+        {
+            int count = sortedDict[i].Value;
+
+            float normalizedHeight = (count / (float)(max))*maxBarHeight;
+
+            Rectangle bar = new Rectangle(
+                startx + i * (barWidth + spacing),
+                basey - (int)normalizedHeight,
+                barWidth,
+                (int)normalizedHeight
+            );
+            //draw created rectangle to the texture (chart)
+            _spriteBatch.Draw(texture, bar, Color.White);
+
+        }
+    }
+    
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+        /*
+        MouseState mouse = Mouse.GetState();
+
+        if (mouse.LeftButton == ButtonState.Pressed && _previousMouse.LeftButton == ButtonState.Released)
+        {
+            DisplayUniqueWords();
+        }
+        
+        _previousMouse = mouse;
+        */
+        _keyboardState = Keyboard.GetState();
+        if (!_keyboardState.IsKeyDown(Keys.Enter))
+        {
+            _held = false;
+        }
+        
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.LightSteelBlue);
+        
+        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        /*
+        for (int i = 0; i < _words.Count; i++)
+        {
+            _spriteBatch.DrawString(
+                _font,
+                _words[i], 
+                _positions[i],
+                _cloudColors[i]
+            );
+        }*/
+        
+        if (display_unique)
+        {
+            window_DisplayUniqueWords(_spriteBatch);
+        }
+        else
+        {
+            DisplayWordFrequency(_spriteBatch, chart);
+        }
         
         _spriteBatch.End();
 
         base.Draw(gameTime);
     }
 }
+
+
+
+
+
